a6d3a66a8cdc7b6c16a5590bb32e5d1c3cec1a25
Davide Cesaroni <davide.cesaroni@gmail.com>
2016-11-16 08:37:21 +0100

    Implement ios app entry

--- index.ios.js
+++ index.ios.js
@@ -8,46 +8,47 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
 
+import Board from './app/components/Board';
+import EndGameOverlay from './app/components/EndGameOverlay';
+
 export default class ReactToe extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.newGame = true;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Board onGameEnded={this.onGameEnded.bind(this)} newGame={this.state.newGame}/>
+        <EndGameOverlay gameWinner={this.state.gameWinner} onRestart={this.restart.bind(this)}/>
       </View>
     );
+  }
+
+  restart() {
+    console.log('restarting');
+    this.setState({gameWinner: null, newGame: true});
+  }
+
+  onGameEnded(gameStatus) {
+    console.log('game ended', gameStatus);
+    this.setState({gameWinner: gameStatus, newGame: false});
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    alignItems: 'center'
+  }
 });
 
 AppRegistry.registerComponent('ReactToe', () => ReactToe);
